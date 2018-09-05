788e1d5b23cbcd014f0c1a35bba7206377bf0ef4
AKubax <ovmiovmiovmi@gmail.com>
2018-09-05 17:39:34 +0300

    akio added (AK input/output)

---
+++ akio.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <utility>
+
+const char* loadFileToBuf(FILE* file, char* buf = 0, unsigned int size = 0);
+
+
+std::pair<const char*, size_t> loadFileToBuf(FILE* file, char* buf, unsigned int size){
+    unsigned int charToRead = size? size : filelength(fileno(file));
+    char* bufToWrite = buf? buf : ((char*) calloc(charToRead, 1));
+
+    _read(fileno(file), bufToWrite, charToRead);
+    return std::pair<const char*, size_t>(bufToWrite, charToRead);
+}
+

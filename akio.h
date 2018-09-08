dc3d0465f62974bc83e60d2505625c6d33e586d9
AKubax <ovmiovmiovmi@gmail.com>
2018-09-08 10:24:10 +0300

    minor akio change

--- akio.h
+++ akio.h
@@ -1,6 +1,7 @@
 #ifndef AKIO_FILE
 #define AKIO_FILE
 
+#include <io.h>
 #include <stdio.h>
 #include <cstdlib>
 #include <utility>
@@ -10,10 +11,10 @@
 
 #define printd(...) { if(DEBUG_LOG_MODE) fprintf(DEBUG_LOG_PATH, __VA_ARGS__); else fprintf(DEBUG_LOG_PATH, "DEBUG OFF\n");}
 
-std::pair<const char*, size_t> loadFileToBuf(FILE* file, char* buf = 0, unsigned int size = 0);
+std::pair<const char*, size_t> writeFileToBuf(FILE* file, char* buf = 0, unsigned int size = 0);
+size_t writeBufToFile(const char* buf, unsigned int size, FILE* file);
 
-
-std::pair<const char*, size_t> loadFileToBuf(FILE* file, char* buf, unsigned int size){
+std::pair<const char*, size_t> writeFileToBuf(FILE* file, char* buf, unsigned int size){
     unsigned int charToRead = size? size : filelength(fileno(file));
     char* bufToWrite = buf? buf : ((char*) calloc(charToRead, 1));
 
@@ -21,6 +22,10 @@ std::pair<const char*, size_t> loadFileToBuf(FILE* file, char* buf, unsigned int
     return std::pair<const char*, size_t>(bufToWrite, charToRead);
 }
 
+
+size_t writeBufToFile(const char* buf, unsigned int size, FILE* file){
+    return fwrite(buf, 1, size, file);
+}
 
 #endif
 

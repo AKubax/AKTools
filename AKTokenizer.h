36c024d00895e1bd3bea9c21a90b3e78c835710e
AKubax <ovmiovmiovmi@gmail.com>
2018-09-16 22:43:57 +0300

    Tokenizer working properly

--- AKTokenizer.h
+++ AKTokenizer.h
@@ -1,8 +1,14 @@
 #ifndef AKTOKENIZER_FILE
 #define AKTOKENIZER_FILE
 
+#include <utility>
+#include <vector>
+#include <string>
 
-#include "AKVector.h"
+#include "akio.h"
+
+#undef DEBUG_LOG_MODE
+#define DEBUG_LOG_MODE 1
 
 struct Token{
     union{
@@ -12,35 +18,77 @@ struct Token{
 
     } value;
 
-    bool isStr = 0;
+    bool isStr;
+
+    Token ():
+        value (),
+        isStr (0)
+    {}
 
     Token (const char* s):
+        value (),
         isStr (1)
     { value.str = s; }
 
     Token (double d):
-        isStr(0)
+        value (),
+        isStr (0)
     { value.dbl = d; }
 };
 
-const char** divide(char* inp, bool skipComments = 1){
-    AKVector<const char*> out;
+std::vector<char*> split(char* inp, bool skipComments = 1);
+std::vector<char*> split(char* inp, bool skipComments/*= 1*/){
+    std::vector<char*> out;
 
     for(char* ptr = inp; *ptr != '\0'; ){
-        if(skipComments) if(ptr[0] == '/' && ptr[1] == '*'){ while( !(ptr[0] == '*' && ptr[1] == '/') ) ptr++; ptr += 2; }
-        while(ptr[0] == ' ' || ptr[0] == '\n') ptr++;
+        while( (ptr[0] == '/' && ptr[1] == '*') || isspace(ptr[0]) ){
+            if(ptr[0] == '\0') return out;
+
+
+            if(skipComments) if(ptr[0] == '/' && ptr[1] == '*'){
+                while( !(ptr[0] == '*' && ptr[1] == '/') ){
+                    ptr++;
+
+                    if(ptr[0] == '\0')  return out;
+                }
+                ptr += 2;
+            }
+
+            while( isspace(ptr[0]) ) ptr++;
+        }
+
+        if(ptr[0] == '\0') return out;
 
         out.push_back(ptr);
-        while(ptr[0] != ' ' && ptr[0] != '\n') ptr++;
+        while( (!isspace(ptr[0])) && ptr[0] != '\0') ptr++;
         ptr[0] = '\0';
-
-        while(ptr[0] == ' ' || ptr[0] == '\n') ptr++;
+        ptr++;
     }
 
-    return out.data();
+    return out;
 }
 
-//Token* tokenize(const char* inp){
+std::vector<Token> tokenize(char* inp, bool skipComments = 1);
+std::vector<Token> tokenize(char* inp, bool skipComments/*= 1*/){
+    std::vector<char*> splitInp = split(inp, skipComments);
+    std::vector<Token> out;
+
+    printd("split() worked successfully\n");
+
+    for(auto& elem : splitInp){
+        char* endPtr = 0;
+        double dblValue = 0;
+
+        dblValue = std::strtod(elem, &endPtr);
+
+        if(endPtr == elem)  out.push_back(      Token(elem));
+        else                out.push_back(  Token(dblValue));
+    }
+
+    return out;
+}
+
+//Token* tokenize(char* inp){
 //    AKVector<Token> out;
 //    bool isComment = 0;
 //
